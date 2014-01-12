af6d6e8d2027bf1afc8455b36972a6ef0ed60417
allenwlee <allenwlee@Allens-MacBook-Pro.local>
2014-01-11 23:31:26 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Awl::Application.config.secret_key_base = '0a13d354868ff235571d11e1fc4a74871cdb822faeb17137d53513005d769b2013af919101435a704220e64ff7f50b48263281a3f2298a0e9ca680be6bafc04d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Awl::Application.config.session_store :cookie_store, key: '_awl_session'
